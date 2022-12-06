a72643006e10149c9df1bbf5afd8ab7004cb2994
danh <thdanh2001@gmail.com>
2022-12-06 21:23:29 +0700

    .

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const server = app.listen(process.env.PORT, () => {
 const io = new Server(server, {
   cors: {
     // origin: "http://localhost:3000",
-    origin: "https://suar-chat.web.app",
+    origin: "http://suar-chat.web.app",
     // credentials: true,
     // transports: ["websocket"],
     // origin: "http://localhost:3005",
